0630b52f8278686ddb3a19e68e317956926819a5
MoschellaV <58868225+MoschellaV@users.noreply.github.com>
2022-09-01 17:57:43 -0400

    -change to personal project #1

--- client/src/components/pages/ProjectsPage/MyProjects.js
+++ client/src/components/pages/ProjectsPage/MyProjects.js
@@ -2,15 +2,16 @@ export const project1 = {
   header: "Project #1",
   subHeader: "CraftACard",
   technologies: {
-    logoNodejs: require("../../../assets/images/codePNGS/node-js.png"),
+    logoMongoDB: require("../../../assets/images/codePNGS/mongodb.png"),
     logoExpress: require("../../../assets/images/codePNGS/express.png"),
     logoReact: require("../../../assets/images/codePNGS/react.png"),
+    logoNodejs: require("../../../assets/images/codePNGS/node-js.png"),
     logoBootstrap: require("../../../assets/images/codePNGS/bootstrap.png"),
   },
   description:
-    "Celebrate the occasion by sending that special someone an eCard. Include your own message and attach your own image. But in case you're feeling uninspired, CraftACard uses multiple API's so you can generate a random image or quote.",
+    "Built with the MERN stack CraftACard allows you to send anyone an eCard with a personal message and image.  But in case you're feeling uninspired, CraftACard uses multiple API's so you can generate a random image or quote.",
   button1Text: "Visit Site",
   button1Href: "https://craftacard.herokuapp.com/",
   button2Text: "View Code",
-  button2Href: "https://github.com/MoschellaV/cardwebsite",
+  button2Href: "https://github.com/MoschellaV/CraftACardWebProject",
 };
